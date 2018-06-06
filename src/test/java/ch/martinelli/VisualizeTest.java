043efe0079f1331633a643679892ee4f5a05e00d
Simon Martinelli <simon@72.services>
2018-06-06 19:29:35 +0200

    new version and first idea how to visualize architecture

---
+++ src/test/java/ch/martinelli/VisualizeTest.java
@@ -0,0 +1,25 @@
+package ch.martinelli;
+
+import com.tngtech.archunit.core.domain.Dependency;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.Test;
+
+public class VisualizeTest {
+
+    JavaClasses classes = new ClassFileImporter().importPackages("ch.martinelli.boundary", "ch.martinelli.control", "ch.martinelli.entity");
+
+    @Test
+    public void draw() {
+        for (JavaClass javaClass : classes) {
+            System.out.println(javaClass.getName());
+            System.out.println("->");
+            for (Dependency dependency : javaClass.getDirectDependenciesFromSelf()) {
+                if (!dependency.getTargetClass().getName().equals(Object.class.getName())) {
+                    System.out.println("   | " + dependency.getTargetClass().getName());
+                }
+            }
+        }
+    }
+}
